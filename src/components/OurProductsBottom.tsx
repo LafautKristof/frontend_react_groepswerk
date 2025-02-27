e3c69b427c09fc4c8a2a88e4bbc1256601c42af4
Sjabloons <kristoflafaut@gmail.com>
2025-02-27 10:00:32 +0100

    debugging pictures

--- src/components/OurProductsBottom.tsx
+++ src/components/OurProductsBottom.tsx
@@ -1,16 +1,29 @@
 import styles from "../css/OurProductsBottom.module.css";
 import ProductCard from "./ProductCard";
+import useSWR from "swr";
+import { useEffect, useState } from "react";
+const fetcher = (url: string) => fetch(url).then((res) => res.json());
 const OurProductsBottom = () => {
+    const {
+        data: products,
+        error,
+        isLoading,
+    } = useSWR(
+        "https://backend-node-groepswerk.onrender.com/api/products/keyboard",
+        fetcher
+    );
+
+    if (error) return <div>Error loading products</div>;
+    if (isLoading) return <div>Loading...</div>;
     return (
         <div className={styles.our_products_bottom}>
-            <ProductCard />
-            <ProductCard />
-            <ProductCard />
-            <ProductCard />
-            <ProductCard />
-            <ProductCard />
-            <ProductCard />
-            <ProductCard />
+            {products &&
+                products.map(
+                    (product: any) => (
+                        console.log(product),
+                        (<ProductCard key={product._id} product={product} />)
+                    )
+                )}
         </div>
     );
 };
--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -1,34 +1,67 @@
 import styles from "../css/ProductCard.module.css";
-const ProductCard = () => {
+
+type ObjectId = string;
+type Product = {
+    _id: ObjectId;
+    name: string;
+    brand: string;
+    price: number;
+    capacity: string;
+    speed: string;
+    type: string;
+    latency: string;
+    rgb: string;
+    description: string;
+    images: string[];
+    raters: string;
+    points: number;
+};
+
+type ProductCardProps = {
+    product: Product;
+};
+
+const ProductCard: React.FC<ProductCardProps> = ({ product }) => {
+    console.log(product);
     return (
         <div className={styles.grid}>
             <div className={styles.image}>
                 <img
-                    className={styles.picture}
-                    src="\src\assets\images\foto_grid.svg"
-                    alt=""
+                    className={styles.images}
+                    src={product?.images[0]}
+                    alt="Product afbeelding"
                 />
                 <div className={styles.cart}></div>
                 <p>-40%</p>
                 <i className={styles.heart}>
-                    <img src="\src\assets\images\Fill_Heart.svg" alt="" />
+                    <img
+                        src={"/src/assets/images/Fill_Heart.svg"}
+                        alt="Favoriet icoon"
+                    />
                 </i>
                 <i className={styles.watch}>
-                    <img src="\src\assets\images\Fill_Eye.svg" alt="" />
+                    <img
+                        src="/src/assets/images/Fill_Eye.svg"
+                        alt="Bekijk product"
+                    />
                 </i>
             </div>
             <div className={styles.info}>
-                <h2>Havit HV-G92 Gamepad</h2>
+                <h2>{product?.name}</h2>
                 <div className={styles.price}>
-                    <p className={styles.new_price}>$ 3.99</p>
-                    <p className={styles.old_price}>$ 7.99</p>
+                    <p className={styles.new_price}>$10.00</p>
+                    <p className={styles.old_price}>$7.99</p>
                 </div>
                 <div className={styles.rating}>
-                    <img src="/src\assets\images\Five_star.png" alt="" />
-                    <p>(88)</p>
+                    <img
+                        src="/src/assets/images/Five_star.png"
+                        alt="Sterrenbeoordeling"
+                    />
+                    <p>(5000)</p>
                 </div>
             </div>
         </div>
     );
 };
+
 export default ProductCard;
