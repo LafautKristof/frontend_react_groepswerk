74323b4f27ef9c44779cbe33fe7e34d2c2c7a843
Sjabloons <kristoflafaut@gmail.com>
2025-03-04 08:59:39 +0100

    024_Add_context_provider

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,43 @@ import Home from "./pages/Home";
 import Login from "./pages/LoginPage";
 import Register from "./pages/RegisterPage";
 
+import { createContext, useState, useEffect } from "react";
+export const Wishlist = createContext([] as any);
+export const Cart = createContext([] as any);
+
 function App() {
+    const [wishlist, setWishlist] = useState(() => {
+        const storedWishlist = localStorage.getItem("wishlist");
+        return storedWishlist ? JSON.parse(storedWishlist) : [];
+    });
+    const addDataToWishlist = () => {
+        localStorage.setItem("wishlist", JSON.stringify(wishlist));
+    };
+
+    const [cart, setCart] = useState(() => {
+        const storedCart = localStorage.getItem("cart");
+        return storedCart ? JSON.parse(storedCart) : [];
+    });
+    const addDataToCart = () => {
+        localStorage.setItem("cart", JSON.stringify(cart));
+    };
+    useEffect(() => {
+        addDataToCart();
+        addDataToWishlist();
+    }, [wishlist, cart]);
+
     return (
-        <Layout>
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/login" element={<Login />} />
-                <Route path="/register" element={<Register />} />
-            </Routes>
-        </Layout>
+        <Cart.Provider value={{ cart, setCart }}>
+            <Wishlist.Provider value={{ wishlist, setWishlist }}>
+                <Layout>
+                    <Routes>
+                        <Route path="/" element={<Home />} />
+                        <Route path="/login" element={<Login />} />
+                        <Route path="/register" element={<Register />} />
+                    </Routes>
+                </Layout>
+            </Wishlist.Provider>
+        </Cart.Provider>
     );
 }
 
