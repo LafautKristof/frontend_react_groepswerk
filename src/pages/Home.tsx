4d943781c7ede5c680194de04462186264ad111b
Sjabloons <kristoflafaut@gmail.com>
2025-03-05 12:33:53 +0100

    add filter

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,12 +5,15 @@ import CategoriesTop from "../components/CategoriesTop";
 import CategoriesBottom from "../components/CategoriesBottom";
 import OurProductsTop from "../components/OurProductsTop";
 import OurProductsBottom from "../components/OurProductsBottom";
+import TopFilter from "../components/TopFilter";
 
 const Home = () => {
     return (
         <>
-            <TopBanner />
-
+            <div className="filter_banner">
+                <TopFilter />
+                <TopBanner />
+            </div>
             <div className="today">
                 <Timer date="2025-03-25T17:00:00" />
                 <Grid1Row />
