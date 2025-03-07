b9f6b021ffd2b15b6ef6a5c3d1395b96ecf83c1f
Sjabloons <kristoflafaut@gmail.com>
2025-03-07 14:25:54 +0100

    028_Link_Filter_component

--- src/components/TopFilter.tsx
+++ src/components/TopFilter.tsx
@@ -1,17 +1,16 @@
+import { Link } from "react-router";
 import styles from "../css/TopFilter.module.css";
 const TopFilter = () => {
     return (
         <>
             <div className={styles.topFilter}>
                 <ul>
-                    <li>lorem</li>
-                    <li>lorem</li>
-                    <li>lorem</li>
-                    <li>lorem</li>
-                    <li>lorem</li>
-                    <li>lorem</li>
-                    <li>lorem</li>
-                    <li>lorem</li>
+                    <Link to="/...">Computerscreen</Link>
+                    <Link to="/...">Headphone</Link>
+                    <Link to="/...">Keayboard</Link>
+                    <Link to="/...">Mouse</Link>
+                    <Link to="/...">RAM</Link>
+                    <Link to="/...">Videocard</Link>
                 </ul>
             </div>
             <div className="seperator"></div>
